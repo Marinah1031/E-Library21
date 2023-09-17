e214c5326067836733c391e3d2f9b6473e9f32fc
Marinah1031 <marinahtam@gmail.com>
2023-09-16 20:34:31 -0700

    getting help and console logging error making new edits to typeDefs and auth.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -139,10 +139,11 @@ console.log(savedBookIds);
                     <Card.Text>{book.description}</Card.Text>
                     {Auth.loggedIn() && (
                       <Button
-                        disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
+                        disabled={savedBookIds?.some(
+                          (savedId) => savedId === book.bookId)}
                         className='btn-block btn-info'
                         onClick={() => handleSaveBook(book.bookId)}>
-                        {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
+                        {savedBookIds?.some((savedId) => savedId === book.bookId)
                           ? 'This book has already been saved!'
                           : 'Save this Book!'}
                       </Button>
--- server/server.js
+++ server/server.js
@@ -24,6 +24,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/'));
+})
 
 const startApolloServer = async () => {
   await server.start();
